fa8d05ab0bf8632741b208d7031bff923718523d
alvNa <alvaro.nav@gmail.com>
2015-01-30 23:10:53 +0100

    Delete wrong classes

--- src/TestPair.java
+++
@@ -1,9 +0,0 @@
-public class TestPair
-{
-	public static void main(String[] args)
-	{
-		Pair<Integer, String> worldCup = new Pair<>(2010, "SouthAfrica");
-		System.out.println("World cup " + worldCup.getFirst() + " in "
-				+ worldCup.getSecond());
-	}
-}
